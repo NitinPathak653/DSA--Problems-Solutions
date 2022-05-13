04ef265d482c8cbab55c19c5a733f483af5e93f3
Nitin Pathak <87492111+NitinPathak7@users.noreply.github.com>
2022-05-13 17:07:05 +0530

    Create Unique_Paths.cpp

---
+++ Unique_Paths.cpp
@@ -0,0 +1,21 @@
+/*  https://leetcode.com/problems/unique-paths/
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner. */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        
+        int N = m + n - 2;
+        int r = m-1;
+        
+        double ans = 1;
+        
+        for(int i = 1; i <= r; i++)
+        {
+            ans = ans * ( N -r + i)/i;
+        }
+        return (int) ans;
+    }
+};
